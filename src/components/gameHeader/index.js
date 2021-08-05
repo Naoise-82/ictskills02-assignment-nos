c6b7212acadadf5ffc86a188f5c955e78befa537
Esioan <esioan667@gmail.com>
2021-08-05 23:26:17 +0100

    navigation arrows now working

--- src/components/gameHeader/index.js
+++ src/components/gameHeader/index.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,13 +17,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const GameHeader = (props) => {
+const GameHeader = ({ game, history}) => {
   const classes = useStyles();
-  const game = props.game;
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back" onClick={() => history.goBack()}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
       <Typography variant="h4" component="h3">
@@ -31,11 +31,11 @@ const GameHeader = (props) => {
           <Typography variant="h5" key={f.id}><b>Franchise:</b> {f.name}</Typography>
         ))}
       </Typography>
-      <IconButton aria-label="go forward">
+      <IconButton aria-label="go forward" onClick={() => history.goForward()}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
   );
 };
 
-export default GameHeader;
+export default withRouter(GameHeader);
